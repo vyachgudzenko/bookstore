5af24971e82bec9e9d5f5a083385e06d2c6d6ab0
vyachgudzenko <vyach.gudzenko@gmail.com>
2020-05-30 19:26:47 +0300

    Add search

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BookListView, BookDetailView
+from .views import BookListView, BookDetailView,SearchResultsListView
 
 app_name = 'books'
 urlpatterns = [
     path('', BookListView.as_view(), name='book_list'),
     path('<int:pk>', BookDetailView.as_view(), name='book_detail'),
+    path('search/',SearchResultsListView.as_view(),name='search_results'),
 ]
--- books/views.py
+++ books/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
 from django.views.generic import ListView,DetailView
 from .models import Book
+from django.db.models import Q
 
 
 class BookListView(LoginRequiredMixin,ListView):
@@ -16,3 +17,13 @@ class BookDetailView(LoginRequiredMixin,PermissionRequiredMixin,DetailView):
     template_name = 'books/book_detail.html'
     login_url = 'account_login'
     permission_required = 'books.special_status'
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+    queryset = Book.objects.filter(title__icontains='beginners')
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+               Q(title__icontains=query)|Q(author__icontains=query))
